3b5ab71a6bcd673e2f7609fce1a301dd96132c78
CreativeRouine <vialiysyvashchenko@gmail.com>
2024-02-02 00:02:58 -0600

    deleted old line

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -4,7 +4,6 @@
 // import Navbar from '@/components/shared/navbar/Navbar'
 // import { Toaster } from '@/components/ui/toaster'
 import React from "react";
-import Example from "@/components/application-ui/application-shells/sidebar/brand_sidebar_with_header";
 import Navbar from "@/components/shared/navbar/Navbar";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
